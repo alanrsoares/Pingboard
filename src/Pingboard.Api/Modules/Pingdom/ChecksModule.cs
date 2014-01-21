c841f2edd3e2a480b8a2e910dc483b892207a85a
Alan R. Soares <alanrsoars@gmail.com>
2014-01-21 15:41:34 -0200

    Implements Nancy.LightiningCace

--- src/Pingboard.Api/Modules/Pingdom/ChecksModule.cs
+++ src/Pingboard.Api/Modules/Pingdom/ChecksModule.cs
@@ -1,7 +1,9 @@
-﻿using Nancy;
-
-namespace Pingboard.Api.Modules.Pingdom
+﻿namespace Pingboard.Api.Modules.Pingdom
 {
+    using System;
+    using Nancy;
+    using Nancy.LightningCache.Extensions;
+
     public class ChecksModule : NancyModule
     {
         public ChecksModule()
@@ -10,13 +12,15 @@ namespace Pingboard.Api.Modules.Pingdom
             Get["/", true] = async (_, ctx) =>
             {
                 var response = await PingdomClient.Pingdom.Client.Checks.GetChecksList();
-                return Response.AsJson(response);
+                return Response.AsJson(response)
+                               .AsCacheable(DateTime.Now.AddSeconds(60));
             };
 
             Get["/{id}", true] = async (_, ctx) =>
             {
                 var response = await PingdomClient.Pingdom.Client.Checks.GetDetailedCheckInformation((int)_.id);
-                return Response.AsJson(response);
+                return Response.AsJson(response)
+                               .AsCacheable(DateTime.Now.AddSeconds(60));
             };
         }
     }
--- src/Pingboard.Api/Startup/Bootstrapper.cs
+++ src/Pingboard.Api/Startup/Bootstrapper.cs
@@ -1,7 +1,9 @@
 ﻿using Nancy;
 using Nancy.Bootstrapper;
 using Nancy.Conventions;
+using Nancy.Routing;
 using Nancy.TinyIoc;
+using Nancy.LightningCache.Extensions;
 using Pingboard.Model.Bundling;
 
 namespace Pingboard.Api.Startup
@@ -14,6 +16,7 @@ namespace Pingboard.Api.Startup
 
             // Setup bundles
             Bundler.Setup();
+            this.EnableLightningCache(container.Resolve<IRouteResolver>(), ApplicationPipelines, new[] { "id", "query", "take", "skip" });
         }
 
         protected override void ConfigureConventions(NancyConventions conventions)
